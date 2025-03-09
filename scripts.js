c998a88bcf31932b30a09cca5aae53eb3616664e
prashanthcodez <hpvictus0324@outlook.com>
2025-03-09 18:36:50 +0530

    Improved Appearance and implemented Sentence case and Alternating case

--- scripts.js
+++ scripts.js
@@ -17,3 +17,22 @@ function convertToTitle() {
     });
     document.getElementById("textOutput").value = output;
 }
+function convertToSentence() {
+    const input = document.getElementById("textInput").value;
+    const output = input.toLowerCase().replace(/(^\s*\w|[.!?]\s*\w)/g, function(txt) {
+        return txt.toUpperCase();
+    });
+    document.getElementById("textOutput").value = output;
+}
+function convertToAlternating() {
+    let textInput = document.getElementById("textInput").value;
+    let alternatingText = '';
+    for (let i = 0; i < textInput.length; i++) {
+        if (i % 2 === 0) {
+            alternatingText += textInput[i].toLowerCase();
+        } else {
+            alternatingText += textInput[i].toUpperCase();
+        }
+    }
+    document.getElementById("textOutput").value = alternatingText;
+}
